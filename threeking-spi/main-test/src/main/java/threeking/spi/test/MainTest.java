55f71ab9ef9fa3caca598f76eb2624f226536ccd
wanghuibs <wanghuibs@enn.cn>
2021-05-20 14:59:08 +0800

    ddd

--- threeking-spi/main-test/src/main/java/threeking/spi/test/MainTest.java
+++ threeking-spi/main-test/src/main/java/threeking/spi/test/MainTest.java
@@ -1,10 +1,13 @@
 package threeking.spi.test;
 
 
+import com.alibaba.fastjson.JSON;
 import com.threeking.spi.service.common.PayService;
 
-import java.util.Iterator;
+
+import java.util.HashMap;
 import java.util.ServiceLoader;
+
 
 /**
  * @Author: A.H
@@ -12,7 +15,60 @@ import java.util.ServiceLoader;
  */
 public class MainTest {
     public static void main(String[] args) {
-        ServiceLoader<PayService> load = ServiceLoader.load(PayService.class);
-        load.forEach(PayService::pay);
+//        ServiceLoader<PayService> load = ServiceLoader.load(PayService.class);
+//        load.forEach(PayService::pay);
+        test();
     }
+
+
+    static void test(){
+
+
+        Order order = new Order();
+
+        order.setId(1);
+
+        order.setName("order1");
+
+//        ListorderDetailList = new ArrayList();
+//
+//        OrderDetail orderDetail = new OrderDetail();
+//
+//        orderDetail.setOrderid(1);
+//
+//        orderDetail.setOrderPrice("1USD");
+//
+//        orderDetail.setOrderSku("Sku1");
+//
+//        orderDetailList.add(orderDetail);
+//
+//        OrderDetail orderDetail2 = new OrderDetail();
+//
+//        orderDetail2.setOrderid(1);
+//
+//        orderDetail2.setOrderPrice("2USD");
+//
+//        orderDetail2.setOrderSku("Sku2");
+//
+//        orderDetailList.add(orderDetail2);
+        String detail = "妖鸟";
+        try {
+            HashMap addMap = new HashMap();
+
+            HashMap addValMap = new HashMap();
+
+            addMap.put("detail", Class.forName("java.lang.String"));
+
+            addValMap.put("detail", detail);
+
+            Object obj2= new ClassUtil().dynamicClass(order,addMap,addValMap);
+
+            System.out.println(JSON.toJSONString(obj2));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
 }
---
+++ threeking-spi/main-test/src/main/java/threeking/spi/test/Order.java
@@ -0,0 +1,25 @@
+package threeking.spi.test;
+
+
+
+public class Order {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private int id;
+
+    private String name;
+}
